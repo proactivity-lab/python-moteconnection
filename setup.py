61e8a50d09ad5428d827ae6e9461318fc14f6524
Raido Pahtma <raidopahtma@gmail.com>
2016-02-10 11:01:23 +0200

    Require pyserial for install.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='moteconnection',
-      version='0.1.0',
+      version='0.1.1',
       description='Python library for using TinyOS inspired serial and tcp connections.',
       url='http://github.com/proactivity-lab/python-moteconnection',
       author='Raido Pahtma',
       author_email='raido.pahtma@ttu.ee',
       license='MIT',
+      install_requires=[
+        "pyserial",
+      ],
       packages=['moteconnection'],
       zip_safe=False)
