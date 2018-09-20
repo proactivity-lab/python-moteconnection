b59616e9ab6cfe7614cfced76bb02ffd1ca24c30
Kaarel Ratas <kaarel.ratas@gmail.com>
2018-09-20 12:18:48 +0300

    Added preliminary test cases.

---
+++ moteconnection/tests/test_serial.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+import codecs
+
+import mock
+import six
+
+
+class IncomingPacketTester(TestCase):
+    def test_incoming_ack_packet(self):
+        """
+        Tests the incoming ack packets over serial.
+        """
+        pass
+
+    def test_incoming_noack_packet(self):
+        """
+        Tests the incoming noack packets over serial.
+        """
+        pass
+
+
+class OutgoingPacketTester(TestCase):
+    def test_outgoing_ack_packet(self):
+        """
+        Tests the outgoing ack packets overs serial.
+        """
+        pass
+
+    def test_outgoing_noack_packet(self):
+        """
+        Tests the outgoing noack packets overs serial.
+        """
+        pass
